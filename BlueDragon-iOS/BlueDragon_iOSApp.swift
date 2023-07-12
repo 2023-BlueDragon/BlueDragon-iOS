8cfcec0b01ef0bbd2cf0ed538c3dd1a5d5993146
Jo-root <whqkrqkr12345@gmail.com>
2023-07-12 11:44:53 +0900

    Initial Commit

---
+++ BlueDragon-iOS/BlueDragon_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlueDragon_iOSApp.swift
+//  BlueDragon-iOS
+//
+//  Created by 조근호 on 2023/07/12.
+//
+
+import SwiftUI
+
+@main
+struct BlueDragon_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
